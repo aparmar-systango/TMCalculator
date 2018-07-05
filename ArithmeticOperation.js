399e67ac270b75d61a01481d5df998946969050d
asharma-systango <asharma@isystango.com>
2018-07-05 14:55:22 +0530

    changed

--- ArithmeticOperation.js
+++ ArithmeticOperation.js
@@ -3,11 +3,11 @@ function multiply(num1, num2)
 	return num1*num2;
 }
 function substract(num1,num2){ 
+	var result;
 	try{   
-    document.getElementById('resultSection').innerHTML='';
-	difference=num1-num2;
+	result=num1-num2;
     }catch(error){
-	alert(error);
+		result=error.message;
     }
     document.getElementById('resultSection').innerHTML=difference;
 	}
