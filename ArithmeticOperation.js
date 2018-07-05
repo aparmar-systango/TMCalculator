ad94c170650a0024d38fdfc9dc78c0611fb562ff
asharma-systango <asharma@isystango.com>
2018-07-05 18:00:49 +0530

    UI jhakas hai

--- ArithmeticOperation.js
+++ ArithmeticOperation.js
@@ -1,11 +1,21 @@
 function add(num1,num2){
 var result;
+if(num1.trim().length==0 ||num2.trim().length==0 ){
+    document.getElementById("resultSection").innerHTML = 'please provide both numbers';
+   return;
+}
+
 result =  num1+num2;
 document.getElementById("resultSection").innerHTML = result;
 }
 
 function division(num1,num2){
     var result;
+    document.getElementById("resultSection").innerHTML = "";
+    if(num1.trim().length==0 ||num2.trim().length==0 ){
+        document.getElementById("resultSection").innerHTML = 'please provide both numbers';
+       return;
+    }
     try
     {
         result = num1/num2;
@@ -18,7 +28,11 @@ function division(num1,num2){
 }
 function substract(num1,num2){ 
 	var result;
-	try{   
+    if(num1.trim().length==0 ||num2.trim().length==0 ){
+        document.getElementById("resultSection").innerHTML = 'please provide both numbers';
+       return;
+    }
+   	try{   
 	result=num1-num2;
     }catch(error){
 		result=error.message;
@@ -31,6 +45,10 @@ function substract(num1,num2){
 function multiply(num1, num2)
 {
 	var result= num1*num2;
-	
+    if(num1.trim().length==0 ||num2.trim().length==0 ){
+        document.getElementById("resultSection").innerHTML = 'please provide both numbers';
+       return;
+    }
+   
 	document.getElementById("resultSection").innerHTML = result.toFixed(2);
 }
