1d7280e420ccfae8f68652d30a8af8b63109b6f5
asharma-systango <asharma@isystango.com>
2018-07-05 14:45:48 +0530

    have add substract

--- ArithmeticOperation.js
+++ ArithmeticOperation.js
@@ -2,3 +2,14 @@ function multiply(num1, num2)
 {
 	return num1*num2;
 }
+function substract(num1,num2){ 
+	try{   
+    document.getElementById('resultSection').innerHTML='';
+	difference=num1-num2;
+    }catch(error){
+	alert(error);
+    }
+    document.getElementById('resultSection').innerHTML=difference;
+	}
+
+	
